8d0329cc69134cc125313927b9c06cdb6980e892
pressdarling <32807920+pressdarling@users.noreply.github.com>
2025-07-12 16:20:53 +1000

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# codex
+
+This plugin provides completions for OpenAI's [Codex](https://github.com/openai/codex) CLI tool.
+
+To use it, add `codex` to the plugins array in your zshrc file.
+
+```
+plugins=(... codex)
+```
+
+**Author:** [@pressdarling](https://github.com/pressdarling)
---
+++ codex.plugin.zsh
@@ -0,0 +1,14 @@
+# Autocompletion for the GitHub CLI (codex).
+if (( ! $+commands[codex] )); then
+  return
+fi
+
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `codex`. Otherwise, compinit will have already done that.
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_codex" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _codex
+  _comps[codex]=_codex
+fi
+
+codex completion zsh >| "$ZSH_CACHE_DIR/completions/_codex" &|
